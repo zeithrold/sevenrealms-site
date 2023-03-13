985c8bbd0285e69a8329992977b4154f623495a9
zeithrold <zeithrold@gmail.com>
2023-03-14 00:27:39 +0800

    reduced font weights

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -4,7 +4,7 @@ import { Box, ThemeProvider } from "@primer/react"
 import { Noto_Sans_SC } from "next/font/google"
 
 const notoSans = Noto_Sans_SC({
-  weight: ["400", "700", "100", "300", "900", "500"],
+  weight: ["700", "900", "500"],
   subsets: ["latin"]
 })
 
