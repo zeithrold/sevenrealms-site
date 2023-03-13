d9e0ace5d7d2c12fbc3d7e349095bb22bcbbcb43
zeithrold <zeithrold@gmail.com>
2023-03-13 20:58:09 +0800

    added department title photos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
       <body
         className={notoSans.className}>
         <Provider>
+          
           <Header />
           {children}
         </Provider>
